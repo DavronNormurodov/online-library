7d8031383804ad8b1b0b9fa15c4e200f74940b73
Davron <mrz.davron@gmail.com>
2021-11-09 10:18:50 +0500

    Update settings.py

--- library-management/settings.py
+++ library-management/settings.py
@@ -43,9 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    
-    'whitenoise.middleware.WhiteNoiseMiddlewaredjango',
-    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
